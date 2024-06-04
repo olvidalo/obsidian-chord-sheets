1c50a5555e19cb2b6431296e4e1313cd69ef301f
Marcel Schaeben <m.schaeben@uni-koeln.de>
2024-06-04 15:49:29 +0200

    rename enharmonic config property for consistency

--- src/chordSheetsSettings.ts
+++ src/chordSheetsSettings.ts
@@ -10,7 +10,7 @@ export interface ChordSheetsSettings {
 	showChordOverview: ShowChordOverviewSetting;
 	showChordDiagramsOnHover: ShowChordDiagramsOnHoverSetting
 	showTransposeControl: boolean;
-    showHarmonicControl: boolean;
+    showEnharmonicControl: boolean;
 	showInstrumentControl: boolean;
 	debug: boolean;
 	defaultInstrument: Instrument;
@@ -25,7 +25,7 @@ export const DEFAULT_SETTINGS: ChordSheetsSettings = {
 	showChordOverview: "always",
 	showChordDiagramsOnHover: "always",
 	showTransposeControl: true,
-    showHarmonicControl: true,
+    showEnharmonicControl: true,
 	showInstrumentControl: true,
 	debug: false,
 	defaultInstrument: "guitar",
