21413b892633629d9b469f5d163ff39197d1ad18
Marcel Schaeben <m.schaeben@uni-koeln.de>
2024-12-15 13:57:36 +0100

    refactor: extract common chord processing logic

--- src/chordProcessing.ts
+++ src/chordProcessing.ts
@@ -2,54 +2,42 @@ import {Chord, Note} from "tonal";
 import {ChordSymbolRange} from "./editor-extension/chordSheetsViewPlugin";
 import {ChangeSpec} from "@codemirror/state";
 
-function transposeNote(chordTonic: string, direction: "up" | "down") {
+export type NoteProcessor = (note: string) => string;
+
+function transposeNote(chordTonic: string, direction: "up" | "down"): string {
 	const transposedTonic = Note.transpose(chordTonic, direction === "up" ? "2m" : "-2m");
 	return direction === "up" ? Note.enharmonic(transposedTonic) : Note.simplify(transposedTonic);
 }
 
-function enharmonicNote(chordTonic: string) {
+function enharmonicNote(chordTonic: string): string {
 	return Note.enharmonic(chordTonic);
 }
 
-export function transpose(chordRanges: ChordSymbolRange[], direction: "up" | "down") {
+export function processChords(chordRanges: ChordSymbolRange[], processNote: NoteProcessor, skipUserDefinedChords = false) {
 	const changes: ChangeSpec[] = [];
 	for (const chordRange of chordRanges) {
-		if (chordRange.chord.userDefinedChord === undefined) {
-			const {from, to, chordSymbol} = chordRange;
-			const [chordTonic, chordType, bassNote] = Chord.tokenize(chordSymbol);
-
-			const simplifiedTonic = transposeNote(chordTonic, direction);
-
-			let transposedChord;
-			if (bassNote) {
-				transposedChord = simplifiedTonic + chordType + "/" + transposeNote(bassNote, direction);
-			} else {
-				transposedChord = simplifiedTonic + (chordType ?? "");
-			}
-
-			changes.push({from: from, to: to, insert: transposedChord});
+		if (skipUserDefinedChords && chordRange.chord.userDefinedChord) {
+			continue;
 		}
+
+		const {from, to, chordSymbol} = chordRange;
+		const [chordTonic, chordType, bassNote] = Chord.tokenize(chordSymbol);
+
+		const processedTonic = processNote(chordTonic);
+		const processedChord = bassNote
+			? processedTonic + chordType + "/" + processNote(bassNote)
+			: processedTonic + chordType;
+
+		changes.push({from: from, to: to, insert: processedChord});
 	}
+
 	return changes;
 }
 
+export function transpose(chordRanges: ChordSymbolRange[], direction: "up" | "down") {
+	return processChords(chordRanges, (note) => transposeNote(note, direction), true);
+}
+
 export function enharmonicToggle(chordTokenRanges: ChordSymbolRange[]) {
-	const changes: ChangeSpec[] = [];
-	for (const chordTokenRange of chordTokenRanges) {
-		const [chordTonic, chordType, bassNote] = Chord.tokenize(chordTokenRange.chordSymbol);
-		const simplifiedTonic = enharmonicNote(chordTonic);
-
-		let enharmonizedChord;
-
-		if (bassNote) {
-			enharmonizedChord = simplifiedTonic + chordType + "/" + enharmonicNote(bassNote);
-		} else {
-			enharmonizedChord = simplifiedTonic + (chordType ?? "");
-		}
-
-		const chordStartIndex = chordTokenRange.from;
-		const chordEndIndex = chordTokenRange.to;
-		changes.push({from: chordStartIndex, to: chordEndIndex, insert: enharmonizedChord});
-	}
-	return changes;
+	return processChords(chordTokenRanges, enharmonicNote);
 }
--- test/chordProcessing.test.ts
+++ test/chordProcessing.test.ts
@@ -1,9 +1,39 @@
-import {transpose} from "../src/chordProcessing";
-import {ChangeSet} from "@codemirror/state";
+import {enharmonicToggle, transpose} from "../src/chordProcessing";
+import {ChangeSet, Text} from "@codemirror/state";
 import {testingSong} from "./data/testing-song";
 import {testingSongInline} from "./data/testing-song-inline";
-import {getChordRangesForSheet, getChordSymbolRangesForLine} from "./utils/utils";
+import {ChordSymbolRange} from "../src/editor-extension/chordSheetsViewPlugin";
+import {isChordToken, tokenizeLine} from "../src/chordsUtils";
 
+export function getChordSymbolRangesForLine(line: string, lineIndex = 0): ChordSymbolRange[] {
+	const {tokens} = tokenizeLine(line, lineIndex, '%c', '%t');
+
+	return tokens
+		.filter(isChordToken)
+		.map(token => ({
+			from: token.index[0] + token.chordSymbolIndex[0],
+			to: token.index[0] + token.chordSymbolIndex[1],
+			chordSymbol: token.chordSymbol,
+			chord: token.chord
+		}));
+}
+
+export function getChordRangesForSheet(sheet: string) {
+	const text = Text.of(sheet.split('\n'));
+	const chordRanges: ChordSymbolRange[] = [];
+	for (let i = 1; i <= text.lines; i++) {
+		const line = text.line(i);
+		const chordRangesForLine = getChordSymbolRangesForLine(line.text, line.from);
+		chordRanges.push(...chordRangesForLine);
+	}
+	return {text, chordRanges};
+}
+
+function enharmonicToggleSheet(sourceSheet: string) {
+	const {text, chordRanges} = getChordRangesForSheet(sourceSheet);
+	const changes = enharmonicToggle(chordRanges);
+	return ChangeSet.of(changes, sourceSheet.length).apply(text).toString();
+}
 
 describe('Transposition', () => {
 
@@ -96,3 +126,23 @@ describe('Transposition', () => {
 
 	});
 });
+
+describe("Enharmonic toggle", () => {
+	test("should toggle enharmonically, simple chords, # -> b", () => {
+		expect(enharmonicToggleSheet("A#m C# G#")).toEqual("Bbm Db Ab");
+	});
+
+	test("should toggle enharmonically, simple chords, b -> #", () => {
+		expect(enharmonicToggleSheet("Bbm Db Ab")).toEqual( "A#m C# G#");
+	});
+
+	test("should toggle enharmonically, complex chords", () => {
+		expect(enharmonicToggleSheet("F#add9 F#7b5 C#maj7 C#6 C#maj7")).toEqual("Gbadd9 Gb7b5 Dbmaj7 Db6 Dbmaj7");
+		expect(enharmonicToggleSheet("Gbadd9 Gb7b5 Dbmaj7 Db6 Dbmaj7")).toEqual("F#add9 F#7b5 C#maj7 C#6 C#maj7");
+	});
+
+	test("should toggle enharmonically, slash chords", () => {
+		expect(enharmonicToggleSheet("C#maj7/D# - A#m7/F# - F#maj7/G# - Fm7/C - A#m7/D# - D#m7/G# - C#maj7/F - C#/G#"))
+			.toEqual("Dbmaj7/Eb - Bbm7/Gb - Gbmaj7/Ab - Fm7/C - Bbm7/Eb - Ebm7/Ab - Dbmaj7/F - Db/Ab");
+	})
+})
